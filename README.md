f7fba0d3768774566ea393203592a4972d2a262f
June <yoo.junehyuk@gmail.com>
2025-03-04 18:06:06 -0600

    Implementing another function in substring_count.rb.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ruby-practice
+
+Practice functions coded in Ruby by yours truly. 
---
+++ substring_count.rb
@@ -0,0 +1,7 @@
+# substring_count.rb
+
+# String that returns a hash of the count of every appearance 
+# of substrings in the dict in our string
+def substring_count(string, dict)
+
+end
